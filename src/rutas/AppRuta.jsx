10ea980b52e2e5ed030b398ae577904ed2d004d7
Leonardo <leonardo@example.com>
2026-05-27 18:24:12 -0500

    Seccion de gastos agregada

--- src/rutas/AppRuta.jsx
+++ src/rutas/AppRuta.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Reportes from '../paginas/Reportes';
 import ReporteFiltros from '../paginas/ReporteFiltros';
 import Perfil from '../paginas/Perfil';
+import Gastos from '../paginas/Gastos';
 import MainLayout from '../componentes/MainLayout';
 
 const AppRuta = () => (
   <BrowserRouter>
     <MainLayout>
       <Routes>
+        <Route path="/expenses" element={<Gastos />} />
         <Route path="/reportes" element={<Reportes />} />
         <Route path="/reporteFiltros" element={<ReporteFiltros />} />
         <Route path="/perfil" element={<Perfil />} />
